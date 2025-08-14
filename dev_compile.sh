69de3a482383c940d3a9b9b526104ad5877f934e
Ascellayn <root.siriusb@gmail.com>
2025-08-14 21:06:05 +0200

    Fix inactive titlebar goofy-ness

--- dev_compile.sh
+++ dev_compile.sh
@@ -2,5 +2,5 @@
 cd /home/ascellayn/Workspace/GitHub/Adellian_Fluent
 ./parse-sass.sh
 ./install.sh
-GTK_DEBUG=interactive thunar
-#GTK_DEBUG=interactive awf-gtk3
+#GTK_DEBUG=interactive thunar
+GTK_DEBUG=interactive awf-gtk3
